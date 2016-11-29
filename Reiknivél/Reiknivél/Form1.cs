7a715a9887f935d052b4b10d3cf6cc499286940d
Bragi Þór Halldórsson <bragiha204@nemi.tskoli.is>
2016-11-29 15:09:44 +0000

    basic reiknivelinn kominn

--- Reiknivél/Reiknivél/Form1.cs
+++ Reiknivél/Reiknivél/Form1.cs
@@ -16,20 +16,201 @@ namespace Reiknivél
         {
             InitializeComponent();
         }
+        string input = null;
+        string firsthluta = null;
+        string seinnihluta = null;
+        string adferd = null;
+        double Reikning = 0;
+        double tala1 = 0, tala2 = 0;
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            rtbUtskrá.Text += "1";
+            input += "1";
+            
+        }
+        private void bt2_Click(object sender, EventArgs e)
+        {
+            rtbUtskrá.Text += "2";
+            input += "2";
+        }
+        private void bt3_Click(object sender, EventArgs e)
+        {
+            rtbUtskrá.Text += "3";
+            input += "3";
+        }
 
-        private void button22_Click(object sender, EventArgs e)
+        private void bt4_Click(object sender, EventArgs e)
+        {
+            rtbUtskrá.Text += "4";
+            input += "4";
+        }
+
+        private void bt5_Click(object sender, EventArgs e)
+        {
+            rtbUtskrá.Text += "5";
+            input += "5";
+        }
+
+        private void bt6_Click(object sender, EventArgs e)
+        {
+            rtbUtskrá.Text += "6";
+            input += "6";
+        }
+
+        private void bt7_Click(object sender, EventArgs e)
+        {
+            rtbUtskrá.Text += "7";
+            input += "7";
+        }
+
+        private void bt8_Click(object sender, EventArgs e)
+        {
+            rtbUtskrá.Text += "8";
+            input += "8";
+        }
+
+        private void bt9_Click(object sender, EventArgs e)
+        {
+            rtbUtskrá.Text += "9";
+            input += "9";
+        }
+        private void btC_Click(object sender, EventArgs e)
+        {
+            rtbUtskrá.Clear();
+            input = null;
+            firsthluta = null;
+            seinnihluta = null;
+        }
+
+        private void btDel_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button16_Click(object sender, EventArgs e)
+        private void btCE_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button19_Click(object sender, EventArgs e)
+        private void btPlus_Click(object sender, EventArgs e)
         {
-
+            rtbUtskrá.Clear();
+            firsthluta = input;
+            input = null;
+            adferd = "+";
+            
         }
+
+        private void btReikna_Click(object sender, EventArgs e)
+        {
+            rtbUtskrá.Clear();
+            seinnihluta = input;
+            input = null;
+            double.TryParse(firsthluta, out tala1);
+            double.TryParse(seinnihluta, out tala2);
+            if (adferd == "+")
+            {
+                if (tala2 == 0)
+                {
+                    Reikning = Reikning + tala1;
+                    rtbUtskrá.Text = Reikning.ToString();
+                }
+                else
+                {
+                    Reikning = tala1 + tala2;
+                    rtbUtskrá.Text = Reikning.ToString();
+
+                }
+                
+            }
+            if (adferd == "-")
+            {
+                Reikning = tala1 - tala2;
+                rtbUtskrá.Text = Reikning.ToString();
+            }
+            if (adferd == "*")
+            {
+                Reikning = tala1 * tala2;
+                rtbUtskrá.Text = Reikning.ToString();
+            }
+            if (adferd == "/")
+            {
+                Reikning = tala1 / tala2;
+                rtbUtskrá.Text = Reikning.ToString();
+            }
+            if (adferd == "%")
+            {
+                Reikning = tala1 * (tala2/100);
+                rtbUtskrá.Text = Reikning.ToString();
+            }
+            if (adferd == "x^x")
+            {
+                Reikning = tala1;
+                for (int i = 1; i < tala2; i++)
+                {
+                    Reikning = Reikning * tala1;
+                }
+                
+                rtbUtskrá.Text = Reikning.ToString();
+            }
+            
+            //seinnihluta = null;
+            //firsthluta = null;
+        }
+
+        private void btMinus_Click(object sender, EventArgs e)
+        {
+            rtbUtskrá.Clear();
+            firsthluta = input;
+            input = null;
+            adferd = "-";
+        }
+
+        private void btMargfoldun_Click(object sender, EventArgs e)
+        {
+            rtbUtskrá.Clear();
+            firsthluta = input;
+            input = null;
+            adferd = "*";
+        }
+
+        private void btDeiling_Click(object sender, EventArgs e)
+        {
+            rtbUtskrá.Clear();
+            firsthluta = input;
+            input = null;
+            adferd = "/";
+        }
+
+        private void btProsent_Click(object sender, EventArgs e)
+        {
+            rtbUtskrá.Clear();
+            firsthluta = input;
+            input = null;
+            adferd = "%";
+        }
+
+        private void btXiveldiX_Click(object sender, EventArgs e)
+        {
+            rtbUtskrá.Clear();
+            firsthluta = input;
+            input = null;
+            adferd = "x^x";
+        }
+
+        private void btXiordu_Click(object sender, EventArgs e)
+        {
+            rtbUtskrá.Clear();
+            firsthluta = input;
+            input = null;
+            adferd = "x^2";
+            double.TryParse(firsthluta, out tala1);
+            Reikning = tala1 * tala1;
+            rtbUtskrá.Text = Reikning.ToString();
+        }
+
+        
+
+        
     }
 }
